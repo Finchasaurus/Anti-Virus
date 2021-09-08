b8a86e59d20d05128580f136f0319900eb966887
Alyx <0822djs@gmail.com>
2021-09-08 11:56:39 -0700

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,7 +1,9 @@
-function FindVirus()
-    if (Virus) then
-        Virus = false
+function FindVirus(space)
+    for _, thing in pairs(space:GetDescendants()) do
+        if thing:IsA("Virus") then
+            Virus:Destroy()
+        end
     end
 end
 
-FindVirus()
+FindVirus(workspace)
