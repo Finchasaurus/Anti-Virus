9a65290513f4a0721cf26a502bcc4a4b9f16d156
Parihsz <65139606+Parihsz@users.noreply.github.com>
2024-08-05 21:28:35 -0400

    use gen iter

--- main.lua
+++ main.lua
@@ -1,5 +1,5 @@
 function FindVirus(space)
-    for _, thing in pairs(space:GetDescendants()) do
+    for _, thing in space:GetDescendants() do
         if thing:IsA("Virus") then
             Virus:Destroy()
         end
